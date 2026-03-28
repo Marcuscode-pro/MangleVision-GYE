0d140f2dcbaeb5c87a695975409fdd433061b2f5
Marcuscode-pro <marcosantoniolopez2007@gmail.com>
2026-03-28 17:38:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ with tab2:
             label2="MANGLEVISION PROPOSAL",
             width = 20000,
             make_responsive=True,
-            starting_position=50
+            
         )
         st.success("✅ Mangrove barriers dissipate up to 40% of wave energy, protecting urban value.")
     else:
